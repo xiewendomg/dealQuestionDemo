90643482a2960b524a5fc5822eb8445e1f0fdf91
unknown <xiewendong@yulintu.local>
2018-11-14 21:03:43 +0800

    HADOOP

---
+++ src/main/scala/com/xiewendomg/admin/spark/中电健康云.scala
@@ -0,0 +1,14 @@
+package com.xiewendomg.admin.spark
+
+import org.apache.spark.sql.SparkSession
+
+object 中电健康云 {
+  val APP_NAME="中电健康云";
+  def main(args: Array[String]): Unit = {
+    //创建spark
+    val spark =SparkSession.builder().appName(APP_NAME).master("local[1]").getOrCreate();
+    //得到sparkContext
+    val sparkContext=spark.sparkContext;
+    val rdd=sparkContext.textFile("classpath:data.txt");
+  }
+}
