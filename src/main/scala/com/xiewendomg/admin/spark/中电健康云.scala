eba881e5fc3d5c84775b4a649a2830786376f3ae
xiewendomg <1344817824@qq.com>
2018-11-15 21:03:35 +0800

    中电健康云

--- src/main/scala/com/xiewendomg/admin/spark/中电健康云.scala
+++ src/main/scala/com/xiewendomg/admin/spark/中电健康云.scala
@@ -3,14 +3,18 @@ package com.xiewendomg.admin.spark
 import org.apache.spark.sql.SparkSession
 
 object 中电健康云 {
-  val APP_NAME="中电健康云";
+  val APP_NAME="中电健康云"
   def main(args: Array[String]): Unit = {
     //创建spark
     val spark =SparkSession.builder().appName(APP_NAME).master("local[1]").getOrCreate()
     //得到sparkContext
     val sparkContext=spark.sparkContext
-    val rdd=sparkContext.textFile("classpath:data.txt")
+    val rdd=sparkContext.textFile("D:\\ideaProject\\dealQuestionDemo\\src\\main\\resources\\data.txt")
+    //map 函数
+    val mapResult1=rdd.map(line=>line.split(",")).collect()
+    //flatMap 函数
+    val mapResult2=rdd.flatMap(line=>line.split(",")).collect()
+    mapResult1.map(x=>(x(0),(x(1),x(2).toDouble)))
 
-    print(rdd)
   }
 }
