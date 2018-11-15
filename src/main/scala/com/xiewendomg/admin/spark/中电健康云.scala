e68d3f107965bd24d6bad24d070dc27ace92cfd2
xiewendomg <1344817824@qq.com>
2018-11-15 14:14:26 +0800

    改变maven仓库

--- src/main/scala/com/xiewendomg/admin/spark/中电健康云.scala
+++ src/main/scala/com/xiewendomg/admin/spark/中电健康云.scala
@@ -6,9 +6,11 @@ object 中电健康云 {
   val APP_NAME="中电健康云";
   def main(args: Array[String]): Unit = {
     //创建spark
-    val spark =SparkSession.builder().appName(APP_NAME).master("local[1]").getOrCreate();
+    val spark =SparkSession.builder().appName(APP_NAME).master("local[1]").getOrCreate()
     //得到sparkContext
-    val sparkContext=spark.sparkContext;
-    val rdd=sparkContext.textFile("classpath:data.txt");
+    val sparkContext=spark.sparkContext
+    val rdd=sparkContext.textFile("classpath:data.txt")
+
+    print(rdd)
   }
 }
